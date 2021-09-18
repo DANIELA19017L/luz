fa1e4c35010384969b6614deece435f8fabce870
karoldanielalopezhiguera@gmail.com <karoldanielalopezhiguera@gmail.com>
2021-09-18 08:49:59 -0500

    goals

---
+++ danielakarol.py
@@ -0,0 +1,12 @@
+numero = [1 ,15,3,4.1,5,-33,7,8,9.5,470]
+"""       0  1  2  3  4  5   6 7 8  9   10   """
+print (numero[5])
+if numero[5] == 0:
+    print("es igual a 0")
+elif numero [5] > 0:    
+    print ("es mayor a 0")
+
+else:
+
+    print("es menor a 0")
+     
